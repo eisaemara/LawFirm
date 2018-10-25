82137c5847c57e0f263c4e4eed80888824b67b3f
Mohamed Emara <dvlpr.mohamedemara@gmail.com>
2018-10-25 21:58:19 +0300

    last chnages

---
+++ LawFirmApp/ViewController/src/com/lawfirm/view/utils/PageDefinationUtils.java
@@ -0,0 +1,11 @@
+package com.lawfirm.view.utils;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+public final class PageDefinationUtils {
+    private PageDefinationUtils() {
+        super();
+    }
+   // public static executeOperation(String operationName , Map params , Runnable onSuccess , Cons)
+}
